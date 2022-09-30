5b1da339d1e9c88855a0640fd6a9d5c9012718be
Yangkai <xkai.yang@ipinyou.com>
2022-09-30 17:21:49 +0800

    leetcode 88

--- src/main/java/com/example/leetcode/list/Coding_88.java
+++ src/main/java/com/example/leetcode/list/Coding_88.java
@@ -44,6 +44,7 @@ public class Coding_88 {
         while(len1 >= 0 && len2 >= 0){
             nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
         }
+        //表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
         System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
 
     }
