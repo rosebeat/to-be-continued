6674a60a634ee642d815ceafaf5b30581abf8064
YoungK <154226491@qq.com>
2022-12-30 23:18:24 +0800

    pom 文件整理

---
+++ src/main/java/com/example/spring/MyRetry.java
@@ -0,0 +1,11 @@
+package com.example.spring;
+
+/**
+ * @tile: MyRetry
+ * @ahthor: kai.Yang
+ * @description:
+ * @date: 2022/12/30 23:13
+ **/
+public class MyRetry {
+
+}
---
+++ src/main/java/com/example/spring/MySpringSevice.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MySpringSevice {
+
+
+
+}
