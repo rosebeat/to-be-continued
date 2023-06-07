90c0ab97a568dbb4653f3f254fd275cd19a33519
YoungK <154226491@qq.com>
2023-06-07 23:17:33 +0800

    pom 添加 jol 显示对象头

--- src/main/java/com/example/spring/MySpringSevice.java
+++ src/main/java/com/example/spring/MySpringSevice.java
@@ -3,11 +3,16 @@ package com.example.spring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 @Slf4j
 public class MySpringSevice {
 
+    public void test(){
 
+
+
+    }
 
 }
